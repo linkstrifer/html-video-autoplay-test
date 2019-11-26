4eb57a8472fbc77fd7b447f3ce574402477822e4
Link Strifer <linkstrifer@outlook.com>
2019-11-25 20:24:18 -0500

    improve UI

--- script.js
+++ script.js
@@ -1,7 +1,63 @@
-const videos = [...document.querySelectorAll("video[data-autoplay]")];
+import data from './videos.json'
 
-videos.forEach(playVideo);
+const { videos } = data
+
+let $container
+const $autoPlayVideos = []
 
 function playVideo(video) {
-  video.play();
+  video.play()
 }
+
+function createVideo({ label, attributes }) {
+  const $details = document.createElement('details')
+  const $summary = document.createElement('summary')
+  const $video = document.createElement('video')
+  const $label = document.createElement('h2')
+  const $attributes = document.createElement('ul')
+
+  Object.keys(attributes).forEach(attributeName => {
+    const $attribute = document.createElement('li')
+    const attributeValue =
+      typeof attributes[attributeName] === 'boolean'
+        ? null
+        : attributes[attributeName]
+
+    $attribute.textContent = `${attributeName}${
+      attributeValue === null ? '' : `="${attributeValue}"`
+    }`
+
+    $attributes.appendChild($attribute)
+
+    $video.setAttribute(
+      attributeName,
+      attributeValue === null ? '' : attributeValue
+    )
+
+    if (attributeName === 'data-autoplay') {
+      $autoPlayVideos.push($video)
+    }
+  })
+
+  $details.classList.add('box')
+
+  $label.textContent = `${label}`
+
+  $summary.appendChild($label)
+  $summary.appendChild($video)
+
+  $details.appendChild($summary)
+  $details.appendChild($attributes)
+
+  $container.appendChild($details)
+}
+
+function init() {
+  $container = document.querySelector('.container')
+
+  videos.forEach(createVideo)
+
+  $autoPlayVideos.forEach(playVideo)
+}
+
+init()
